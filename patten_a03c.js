73a7141d033c028a204bb88329826b7944e3e90d
Bapatten <58838719+Bapatten@users.noreply.github.com>
2019-12-21 21:33:03 -0600

    Add files via upload

---
+++ patten_a03c.js
@@ -0,0 +1,14 @@
+/*Bryan Patten
+patten_a03c.js
+INFO 2124
+Lisa Thoendel
+12/20/2019*/
+
+let str = "When in the course of human events";
+let message = `
+    The preamble is:
+        ${ str } 
+        
+    Does the preamble contain 'a'?
+        ${ str.includes("a") }`
+console.log(message);
